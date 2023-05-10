96d5c3b0e1ea967fe0f9934dd0622b144e5dbdaa
AlexanderPixel <tkachenkosasha17@gmail.com>
2023-05-10 20:49:41 +0200

    [add] added interactive mode for equations

---
+++ interactive_mode.py
@@ -0,0 +1,18 @@
+from algorithm import solve
+
+def entry (coeff):
+    response = input(f'{coeff} = ')
+    try:
+        number = float(response)
+    except:
+        print(f'Error. Expected a valid real number, got {response} instead')
+        return entry(coeff)
+    return number
+
+def main():
+    a, b, c = entry('a'), entry('b'), entry('c')
+    if a:
+        return solve(a, b, c)
+    else:
+        print('a cannot be 0')
+        return main()

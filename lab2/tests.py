13fcdfed06530ad72f47fa1760bbeb016f181ff2
AlexanderPixel <tkachenkosasha17@gmail.com>
2023-05-15 21:54:28 +0200

    [add] added tests for linked list

---
+++ lab2/tests.py
@@ -0,0 +1,166 @@
+import unittest
+import list
+
+class TestLinkedList(unittest.TestCase):
+
+    list = list.LinkedList()
+
+    def test_append(self):
+        #arrange
+        ls = list.LinkedList()
+        value = "1"
+
+        #act
+        ls.append(value)
+
+        #assert
+        self.assertEqual(ls.get(0), value)
+
+    def test_insert(self):
+        #arrange
+        ls = list.LinkedList()
+
+        #act
+        ls.append('4')
+        ls.insert(0, '0')
+        ls.print()
+
+        #arrange
+        self.assertEqual(ls.get(1), '4')
+
+        with self.assertRaises(IndexError):
+            ls.insert(ls.getLength(), 'Invalid Value')
+
+    def test_length(self):
+        ls = list.LinkedList()
+
+        ls.append('1')
+        ls.append('2')
+        ls.append('3')
+
+        self.assertEqual(ls.getLength(), 3)
+
+    def test_get(self):
+        #arrange
+        ls = list.LinkedList()
+
+        ls.append('1')
+        ls.append('2')
+        ls.append('3')
+        
+        #act
+        value = ls.get(0)
+        #assert
+        self.assertEqual(value, '1')
+
+        with self.assertRaises(IndexError):
+            ls.get(ls.getLength() + 1)
+
+    def test_clear(self):
+
+        ls = list.LinkedList()
+        
+        ls.append('1')
+        ls.append('2')
+        ls.append('3')
+        
+        ls.clear()
+
+        self.assertEqual(ls.getLength(), 0)
+
+    def test_delete(self):
+        ls = list.LinkedList()
+        
+        ls.append('1')
+        ls.append('2')
+        ls.append('3')
+
+        deleted = ls.delete(0)
+
+        self.assertEqual(ls.getLength(), 2)
+        self.assertEqual(deleted, '1')
+
+        with self.assertRaises(IndexError):
+            ls.delete(ls.getLength() + 1)
+
+    def test_delete_all(self):
+        ls = list.LinkedList()
+        
+        ls.append('1')
+        ls.append('3')
+        ls.append('3')
+
+        ls.deleteAll('3')
+
+        self.assertEqual(ls.getLength(), 1)
+
+    def test_find_first(self):
+        ls = list.LinkedList()
+        
+        ls.append('3')
+        ls.append('1')
+        ls.append('3')
+
+        first = ls.findFirst('3')
+
+        self.assertEqual(first, 0)
+
+    def test_find_last(self):
+        ls = list.LinkedList()
+        
+        ls.append('3')
+        ls.append('1')
+        ls.append('3')
+
+        last = ls.findLast('3')
+
+        self.assertEqual(last, 2)
+
+    def test_reverse(self):
+        ls = list.LinkedList()
+
+        ls.append('1')
+        ls.append('2')
+        ls.append('3')
+
+        ls.reverse()
+
+        self.assertEqual(ls.get(0), '3')
+
+    def test_clone(self):
+        ls = list.LinkedList()
+
+        ls.append('1')
+        ls.append('2')
+        ls.append('3')
+
+        clone = ls.clone()
+
+        self.assertEqual(ls.get(0), clone.get(0))
+
+    def test_extend(self):
+        ls1 = list.LinkedList()
+        ls2 = list.LinkedList()
+
+        ls1.append('First list')
+        ls2.append('Second list')
+
+        ls1.extend(ls2)
+
+        self.assertEqual(ls1.get(1), 'Second list')
+
+    def test__get_node(self):
+        ls = list.LinkedList()
+        ls.append('2')
+        ls.append('4')
+
+        node = ls._get_node(1)
+
+        self.assertEqual(node.element, '4')
+
+        with self.assertRaises(IndexError):
+            ls._get_node(ls.getLength() + 2)
+
+
+if __name__ == '__main__':
+    unittest.main()

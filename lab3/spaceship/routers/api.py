46a04853b67c2dfd7ea4228e6c34625ee3a6aa1b
AlexanderPixel <tkachenkosasha17@gmail.com>
2023-05-18 21:07:59 +0200

    [add] added new endpoint

--- lab3/spaceship/routers/api.py
+++ lab3/spaceship/routers/api.py
@@ -6,3 +6,19 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World! It is Alex Tkachenko from IM-11'}
+
+
+@router.get('/matrix')
+def matrix() -> dict:
+    a = np.random.rand(10, 10)
+    b = np.random.rand(10, 10)
+
+    result = np.dot(a, b)
+
+    response = {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product": result.tolist()
+    }
+
+    return response
